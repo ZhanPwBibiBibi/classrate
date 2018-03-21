f4009ae71be11642ede66cde6c44794603880fbc
ZhanPw <zhanpengwei@e.gzhu.edu.cn>
2018-03-21 17:00:03 +0800

    GZHU spider
    Add get credit support

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ if __name__ == '__main__':
     student.show_personal_score()
     student.show_class_table()
     student.score_pdf_generate()
+    student.get_personal_credit_required()
+    student.get_personal_credit_electives()
+    student.show_credit_required()
+    student.show_credit_electives()
 
     # Don't forget this
     student.exit()
